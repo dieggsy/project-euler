180bde74596a5bae23dc2df989bdf1ca676afdfa
dieggsy <dieggsy@protonmail.com>
2019-01-29 22:53:04 -0500

    Extremely lazy commit - headers, timers, cleanup
    Use number headers for convenience functions, use Timer class more,
    clean up/refactor some code.

---
+++ numbers.h
@@ -0,0 +1,64 @@
+// -*- mode: cpp -*-
+#include <cmath>
+#include <type_traits>
+
+template <
+    typename T,
+    typename = std::enable_if<std::is_arithmetic<T>::value>
+    >
+bool small_prime_p(T x) {
+    if (x <= 1 ) { return false; }
+    else if (x <= 3) { return true; }
+    else if (x % 2 == 0 || x % 3 == 0) { return false; }
+    else {
+        int i = 5;
+        while (true) {
+            if (std::pow(i,2) > x) {
+                return true;
+            }
+            else if (x % i == 0 || x % (i + 2) == 0) {
+                return false;
+            }
+            ++i;
+        }
+    }
+}
+
+template <
+    typename T,
+    typename = std::enable_if<std::is_arithmetic<T>::value>
+    >
+T nth_digit(T m, int n) {
+    return ((m % static_cast<T>(std::pow(10,n + 1))) - (m % static_cast<T>(std::pow(10, n))))
+        / static_cast<T>(std::pow(10, n));
+}
+
+template <
+    typename T,
+    typename = std::enable_if<std::is_arithmetic<T>::value>
+    >
+T digit_sum(T n) {
+    T pow = 0;
+    T res = 0;
+    while (std::pow(10,pow) <= n) {
+        T digit = nth_digit(n, pow);
+        res += digit*digit;
+        ++pow;
+    }
+    return res;
+}
+
+template <
+    typename T,
+    typename = std::enable_if<std::is_arithmetic<T>::value>
+    >
+T factorial(T n) {
+    T result = 1;
+    if (n == 0) {
+        return result;
+    }
+    for (int i = n; i >= 1;--i) {
+        result *= i;
+    }
+    return result;
+}
