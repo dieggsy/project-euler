7e5691db6739a1c07f90560c3fa37e158e23c1d5
dieggsy <dieggsy@protonmail.com>
2019-02-07 14:04:56 -0500

    Parallelize p357

--- rust/src/bin/p357.rs
+++ rust/src/bin/p357.rs
@@ -11,11 +11,11 @@ fn main() {
     {
         !(1..n).take_while(|&i| i.pow(2) <= n)
             .filter(|&i| n % i == 0)
-            .any(|d| !sieve.is_prime((d + n/d) as usize) // !small_prime_p(d+n/d)
+            .any(|d| !sieve.is_prime((d + n/d) as usize)
             )
     };
 
-    let res: u64 = (1u64..100_000_000)// .into_par_iter()
+    let res: u64 = (1u64..100_000_000).into_par_iter()
         .filter(|&n| special(n as u32)).sum();
     // let res = is_prime[ 31 ];
     // let res = special(30);
