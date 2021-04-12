9cceee6691db78dd7756237b0ab6eef915e316d4
dieggsy <dieggsy@pm.me>
2021-04-12 11:39:53 -0400

    Remove debug print, add time/result for p63

--- chicken/p63.scm
+++ chicken/p63.scm
@@ -1,11 +1,12 @@
 #!/usr/bin/chicken-scheme
+;; AUTOCOMPILE: -O3
 
 (define (p63)
   (let loop ((a 1))
     (let loop1 ((b 1))
-      (let* ((n (expt a b))
-             (l (string-length (number->string n))))
-        (print a "^" b "=" n)
+      (let* ((l (string-length (number->string (expt a b)))))
         (cond ((> a 9) 0)
               ((< l b) (loop (+ a 1)))
               ((= l b) (+ 1 (loop1 (+ b 1)))))))))
+
+(time (print (p63)))
